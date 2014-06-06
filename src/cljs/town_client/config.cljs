48bf57f2241798310a56227ba98733e9b80aba64
Timo Tuominen <tituomin@gmail.com>
2014-06-06 15:39:18 +0300

    Formatting.

--- src/cljs/town_client/config.cljs
+++ src/cljs/town_client/config.cljs
@@ -1,6 +1,7 @@
 (ns town-client.config)
 
 (def url-base "http://dev.hel.fi/kenenkaupunki/api/")
+;(def url-base "http://localhost:8079/")
 
 (def aggregates
   {:asuminen #{"paikka-tai-alue-asuinrakentamiselle"
@@ -9,4 +10,5 @@
                "taalla-pitaisi-olla-enemman-kauppoja-ja-palveluita-rakennusten-kivijaloissa"}
   :virkistys #{"virkistyksellisesti-tarkea-mutta-saisi-olla-laadultaan-parempi"
                 "taalla-on-tallaisenaan-ainutlaatuista-kaupunkiluontoa"}
-  :ankeus #{"taalla-ymparisto-nayttaa-ankealta-ja-sita-pitaisi-parantaa-esimerkiksi-puuistutuksin" "huonosti-hoidettu-epamaarainen-alue-jota-tulisi-parantaa"}})
+  :ankeus #{"taalla-ymparisto-nayttaa-ankealta-ja-sita-pitaisi-parantaa-esimerkiksi-puuistutuksin"
+            "huonosti-hoidettu-epamaarainen-alue-jota-tulisi-parantaa"}})
